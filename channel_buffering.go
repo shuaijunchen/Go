56e28d07da3a6b185c51e7731ca1fd229b233662
shuaijunchen <shuaijunchen@hotmail.com>
2016-12-24 17:38:39 +0800

    通道缓冲

---
+++ channel_buffering.go
@@ -0,0 +1,16 @@
+// channel_buffering.go
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	message := make(chan string, 2)
+
+	message <- "buffered"
+	message <- "channel"
+
+	fmt.Println(<-message)
+	fmt.Println(<-message)
+}
