aed3f4c02e8997f38335b1cf3cf82a912ebc5b66
shuaijunchen <shuaijunchen@hotmail.com>
2017-01-02 22:08:44 +0800

    go 文件上传

--- main.go
+++ main.go
@@ -1,54 +1,106 @@
-// main.go
 package main
 
 import (
-	"encoding/json"
+	// "fmt"
+	"html/template"
+	"io"
+	"log"
 	"net/http"
+	"os"
 )
 
-type User struct {
-	Name         string `json:"name"`
-	Age          int    `json:"age"`
-	Gender       int    `json:"gender"`
-	City         string `json:"city"`
-	Country      string `json:"country"`
-	Country_Code int    `json:"countryCode"`
-}
+const (
+	UPLOAD_DIR = "./uploads"
+)
 
-type Message struct {
-	Code   int    `json:"code"`
-	Status string `json:"status"`
-	Data   []User `json:"data"`
-}
-
-func foo(w http.ResponseWriter, r *http.Request) {
-	user1 := User{"张三", 20, 1, "成都", "CHN", 156}
-	user2 := User{"李四", 21, 1, "北京", "CHN", 156}
-	user3 := User{"王五", 22, 1, "上海", "CHN", 156}
-	user4 := User{"赵六", 22, 1, "深圳", "CHN", 156}
-	user5 := User{"Tom", 21, 1, "New York", "USA", 840}
-	user6 := User{"Tony", 20, 1, "London", "GBR", 826}
-	message := Message{200, "OK", []User{user1, user2, user3, user4, user5, user6}}
-
-	js, err := json.Marshal(message)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		t, err := template.ParseFiles("upload.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		t.Execute(w, nil)
 		return
 	}
+	if r.Method == "POST" {
+		// 获取上传信息
+		f, h, err := r.FormFile("file")
+		log.Println(f)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		// 获取文件名
+		filename := h.Filename
+		log.Println(filename)
+		defer f.Close() // 关闭
 
-	w.Header().Set("server", "A Go Web Server")
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
+		// 文件路径
+		path := UPLOAD_DIR + "/" + filename
+		log.Println(path) //文件路径
+
+		// 获取当前目录
+		dir, _ := os.Getwd()
+		log.Println(dir) //当前文件目录
+
+		// a, err := os.Stat(UPLOAD_DIR) // 检查文件是否存在
+		isV := isExists(UPLOAD_DIR)
+		if !isV {
+			// 创建文件夹
+			e := os.Mkdir(dir+"/"+UPLOAD_DIR, os.ModePerm)
+			if e != nil {
+				log.Println(e)
+			}
+			log.Println("创建成功")
+		}
+
+		// 创建文件
+		t, err := os.Create(path)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer t.Close() //关闭
+
+		if _, err := io.Copy(t, f); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		// 重定向到上传成功页面
+		http.Redirect(w, r, "/view?id="+filename, http.StatusFound)
+	}
 }
 
-func hello(w http.ResponseWriter, r *http.Request){
-	// w.Header().Set("server", "A Go Web Server")
-	w.Write([]byte("Hello, 世界！"))
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	// 获取文件信息
+	fileId := r.FormValue("id")
+	// 拼接文件路径
+	filePath := UPLOAD_DIR + "/" + fileId
+	if exists := isExists(filePath); !exists {
+		http.NotFound(w, r)
+		return
+	}
+	w.Header().Set("Content-Type", "file")
+	http.ServeFile(w, r, filePath)
 }
 
+// true:文件存在，false:文件不存在
+func isExists(path string) bool {
+	// 检查文件是否存在
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	}
+	return os.IsExist(err)
+}
 
 func main() {
-	go http.HandleFunc("/message", foo)
-	go http.HandleFunc("/hello", hello)
-	http.ListenAndServe(":8090", nil)
+	http.HandleFunc("/view", viewHandler)
+	http.HandleFunc("/upload", uploadHandler)
+	// 监听端口
+	err := http.ListenAndServe(":8090", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err.Error())
+	}
 }
