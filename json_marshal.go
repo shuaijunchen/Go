d29d930d6576086dc09e2a4eb16c23f35122a6c1
shuaijunchen <228377627@qq.com>
2017-03-12 22:37:23 +0800

    JSON 数据加密解密

---
+++ json_marshal.go
@@ -0,0 +1,23 @@
+// json.go
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+func main() {
+	h := json.RawMessage(`{"precomputed":true}`)
+	c := struct {
+		Header *json.RawMessage `json:"header"`
+		Body   string           `json:"body"`
+	}{Header: &h, Body: "Hello Gopers!"}
+
+	b, err := json.MarshalIndent(&c, "", "\t")
+
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	os.Stdout.Write(b)
+}
