777901d2d39614baf6a5cb79718a28f203f71824
Charles.Chen <toshuaijun@gmail.com>
2017-04-19 21:44:17 +0800

    Golang Channel for range

---
+++ goroutine/demo1/channel_range.go
@@ -0,0 +1,19 @@
+// channel_range.go
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	ch := make(chan bool, 2)
+
+	ch <- true
+	ch <- true
+
+	close(ch)
+
+	for v := range ch {
+		fmt.Println(v)
+	}
+}
