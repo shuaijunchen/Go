f2f927329a5e32a5b1a4ac8644d78902b08f9059
Charles.Chen <toshuaijun@gmail.com>
2017-06-18 23:22:38 +0800

    git new create branch redis2

--- redis/connection.go
+++ redis/connection.go
@@ -4,12 +4,11 @@ package main
 import (
 	"fmt"
 	"github.com/garyburd/redigo/redis"
-	"time"
 )
 
 const (
-	host = "127.0.0.1:6379"
-	auth = "password"
+	host = "192.168.99.100:6379"
+	auth = "123456"
 )
 
 func main() {
@@ -26,33 +25,10 @@ func main() {
 	_, err = c.Do("AUTH", auth)
 	errorCheck("redis auth error:", err)
 
-	// 写入值永远不过期
-	// _, err = c.Do("SET", "username", "zhangsan")
-	// errorCheck("redis set failed:", err)
-
-	key, err := c.Do("EXISTS", "username")
-	fmt.Println(key)
-
-	// 删除 username
-	_, err = c.Do("DEL", "username")
-	errorCheck("reids delete failed", err)
-	fmt.Println("Delte redis key Success!")
-
-	// 获取 username 的值
-	username, err := redis.String(c.Do("GET", "username"))
+	// 获取 shuaijunchen 的值
+	username, err := redis.String(c.Do("GET", "shuaijunchen"))
 	errorCheck("redis get failed:", err)
 	fmt.Printf("Got username %v \n", username)
-
-	// 写入值 10S 后过期
-	_, err = c.Do("SET", "password", "123456", "EX", "1")
-	errorCheck("redis set failed:", err)
-
-	// 睡11S
-	time.Sleep(2 * time.Second)
-
-	password, err := redis.String(c.Do("GET", "password"))
-	errorCheck("redis get failed:", err)
-	fmt.Printf("Got password %v \n", password)
 }
 
 func errorCheck(msg string, err error) {
