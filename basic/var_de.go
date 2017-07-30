a340d5076af7c17e75f4775741e88483939bc17b
Charles <toshuaijun@gmail.com>
2017-07-30 14:40:02 +0800

    Go 变量定义

---
+++ basic/var_de.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 变量定义
+	var i int
+	fmt.Println(i)
+
+	// 定义多个变量
+	var v1, v2, v3 string
+	fmt.Println(v1, v2, v3)
+
+	// 定义变量并赋值
+	var str string = "张三"
+	fmt.Println(str)
+
+	// 定义变量，分别初始化相应的值，类型会根据初始值自动推导出想要的类型
+	ii, f, s, d := 10, 20.01, "string", true
+	fmt.Println(ii, f, s, d)
+	fmt.Printf("Type i type: %T\n", ii) // 推导出变量类型
+	fmt.Printf("Type f type: %T\n", f)
+	fmt.Printf("Type s type: %T\n", s)
+	fmt.Printf("Type d type: %T\n", d)
+
+}
